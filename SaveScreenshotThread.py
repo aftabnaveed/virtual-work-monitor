b3356d26c156d659874e262c38a902de68cf0073
Aftab Naveed <aftab@renegadefurniture.com>
2014-12-12 20:33:25 +1100

    [FEATURE] Seprate thread to save the generated screenshot

---
+++ SaveScreenshotThread.py
@@ -0,0 +1,22 @@
+#/usr/bin/env python
+
+
+from PyQt4 import QtCore
+
+class SaveSchreenshotThread(QtCore.QThread):
+    
+    capture = QtCore.pyqtSignal(object)
+    screenshot = None
+    imageFormat = 'png'
+    fileName = None
+    pixelMap = None
+
+    def __init__(self, screenshot, pixelMap, fileName, imageFormat='png'):
+        QtCore.__init__(self)
+        self.screenshot = screenshot
+        self.imageFormat = imageFormat
+        self.fileName = fileName
+        self.pixelMap = pixelMap
+
+    def run(self):
+        self.capture.emit(self.screenshot, self.filenName, self.imageFormat)
